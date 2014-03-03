4e586e3150165bbb36ed82b60ff49d0f0538b9c4
planetClaire <marshy@zeta.com.au>
2014-03-04 05:42:32 +0800

    Add & fix ImageField module

---
+++ src/Orchard.Web/Modules/Contrib.ImageField/Settings/ImageFieldSettings.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Contrib.ImageField.Settings {
+    public class ImageFieldSettings {
+        [Range(0, 2048)]
+        public int MaxWidth { get; set; }
+        [Range(0, 2048)]
+        public int MaxHeight { get; set; }
+        public ResizeActions ResizeAction { get; set; }
+        public bool Required { get; set; }
+        public bool AlternateText { get; set; }
+        public string Hint { get; set; }
+        public string MediaFolder { get; set; }
+        public string FileName { get; set; }
+    }
+
+    public enum ResizeActions {
+        // Ensures the original image is in the boundaries
+        Validate,
+        // Apply binary transformation so that the new image complies with boundaries
+        Resize,
+        // Don't alter the image, the html code will define the size to render
+        Scale,
+        // If the image is out of bounds, the image is cropped
+        Crop
+    }
+}
---
+++ src/Orchard.Web/Modules/Contrib.ImageField/ViewModels/ImageFieldViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Contrib.ImageField.Settings;
+
+namespace Contrib.ImageField.ViewModels {
+    public class ImageFieldViewModel {
+        public ImageFieldSettings Settings { get; set; }
+        public Fields.ImageField Field { get; set; }
+        public string AlternateText { get; set; }
+        public bool Removed { get; set; }
+    }
+}
