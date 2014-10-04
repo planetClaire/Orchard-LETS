c7722d949007be8a7c2b9d4989e17f27b5da7ce0
marshy <marshy@zeta.com.au>
2014-10-04 13:28:20 +0800

    Add and trigger an unpublished workflow activity so we can send members an email when their notices are archived.

---
+++ src/Orchard.Web/Modules/LETS/Workflow/ContentUnpublishedActivity.cs
@@ -0,0 +1,16 @@
+﻿using Orchard.Localization;
+using Orchard.Workflows.Activities;
+
+namespace LETS.Workflow
+{
+    public class ContentUnpublishedActivity : ContentActivity
+    {
+        public override string Name {
+            get { return "ContentUnpublished"; }
+        }
+
+        public override LocalizedString Description {
+            get { return T("Content is unpublished."); }
+        }
+    }
+}
---
+++ src/Orchard.Web/Modules/LETS/Workflow/UnpublishHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Orchard.ContentManagement;
+using Orchard.ContentManagement.Handlers;
+using Orchard.Workflows.Services;
+
+namespace LETS.Workflow
+{
+    public class UnpublishHandler : ContentHandler
+    {
+        public UnpublishHandler(IWorkflowManager workflowManager)
+        {
+            OnUnpublished<ContentPart>(
+                (context, part) =>
+                    workflowManager.TriggerEvent("ContentUnpublished",
+                    context.ContentItem,
+                    () => new Dictionary<string, object> { { "Content", context.ContentItem } }));
+        }
+    }
+}
