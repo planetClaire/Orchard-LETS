793091e7bea2a5d10c68399b15da95848a9f33b4
marshy <marshy@zeta.com.au>
2016-03-02 16:43:43 +0800

    add notices admin view

--- src/Orchard.Web/Modules/LETS/Controllers/NoticesAdminController.cs
+++ src/Orchard.Web/Modules/LETS/Controllers/NoticesAdminController.cs
@@ -1,19 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
+using LETS.Models;
+using LETS.Services;
 using LETS.ViewModels;
+using Orchard.ContentManagement;
+using Orchard.Security;
 using Orchard.UI.Admin;
 
 namespace LETS.Controllers
 {
     public class NoticesAdminController : Controller
     {
+        private readonly INoticeService _noticeService;
+        private readonly IContentManager _contentManager;
+
+        public NoticesAdminController(INoticeService noticeService, IContentManager contentManager) {
+            _noticeService = noticeService;
+            _contentManager = contentManager;
+        }
+
         [Admin]
-        public ActionResult List(int id)
-        {
-            return View();
+        public ActionResult List(int id) {
+            var member = _contentManager.Get(id, VersionOptions.Latest).As<MemberPart>();
+            var memberNoticesViewModel = new MemberNoticesViewModel { Notices = _noticeService.GetMemberNoticeShapes(id), ArchivedNotices = _noticeService.GetMemberArchivedNoticeShapes(id), Member = member, AdminIsViewing = true };
+            return View(memberNoticesViewModel);
         }
     }
 }
