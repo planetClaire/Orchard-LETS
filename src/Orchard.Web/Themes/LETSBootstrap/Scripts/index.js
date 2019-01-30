7d0530b20ed61dcdc37f24eb5346c6bd842692a7
marshy <marshy@zeta.com.au>
2019-01-30 11:23:13 +0800

    Begin move of jquery unobtrusive validation out of nuget & LETS module & into npm & webpack in LETSBootstrap theme - Themes/LETSBootstrap/Views/EditorTemplates/Parts.Title.TitlePart.cshtml shows how to make bootstrap4 validation work with jquery unobtrusive

--- src/Orchard.Web/Themes/LETSBootstrap/Scripts/index.js
+++ src/Orchard.Web/Themes/LETSBootstrap/Scripts/index.js
@@ -1,4 +1,6 @@
 import 'bootstrap';
+import 'jquery-validation';
+import 'jquery-validation-unobtrusive';
 require("../scss/app.scss");
 if (process.env.NODE_ENV !== 'production') {
     console.log('Looks like we are in development mode!');
