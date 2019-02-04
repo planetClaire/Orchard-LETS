117829e4db51f3071f90500750696d11042f1d8d
marshy <marshy@zeta.com.au>
2019-02-04 13:00:44 +0800

    override transaction & archive notice editor to get rid of jqueryui & move scripts into theme's webpack

--- src/Orchard.Web/Themes/LETSBootstrap/Scripts/index.js
+++ src/Orchard.Web/Themes/LETSBootstrap/Scripts/index.js
@@ -1,6 +1,9 @@
 import 'bootstrap';
 import 'jquery-validation';
 import 'jquery-validation-unobtrusive';
+import flatpickr from "flatpickr";
+import * as $ from "jquery";
+
 require("../scss/app.scss");
 
 if (process.env.NODE_ENV !== 'production') {
@@ -24,3 +27,51 @@ window.addEventListener('load', function () {
 }, false);
 
 $(".wrap-row").wrapAll("<div class='no-gutters row'/>");
+
+flatpickr(".transaction-datepicker", {
+    dateFormat: 'd-m-Y',
+    maxDate: $("#today").val(),
+    minDate: $("#minDate").val()
+});
+
+let archiveDatePicker = flatpickr(".archive-datepicker", {
+    dateFormat: 'd-m-Y',
+    minDate: $("#today").val(),
+    maxDate: $("#maxDate").val()
+});
+
+$("#archiveQuickPick a.btn").click(function (event) {
+    event.preventDefault();
+    var days = $(this).children("input:hidden").val();
+    var today = new Date();
+    var pickedDay = new Date(today.getFullYear(), today.getMonth(), today.getDate() + parseInt(days));
+    $(".archive-datepicker").val(flatpickr.formatDate(pickedDay, 'd-m-Y'));
+    archiveDatePicker.setDate(pickedDay);
+    $("#archiveQuickPick a.btn").each(function () {
+        $(this).removeClass('active');
+    })
+    $(this).addClass('active');
+});
+
+// ensure the default archive notice value is sent if date has no value
+$(".edit-create-notice button:submit").click(function () {
+    if ($(".archive-datepicker").val() === '') {
+        $(".archive-datepicker").val($("#maxDate").val());
+    }
+});
+
+// highlight relevant archive notice buttons
+$(function () {
+    $("#archiveQuickPick").each(function () {
+        var dateVal = $(".archive-datepicker").val();
+        if (dateVal === $("#maxDate").val()) {
+            $("#aMaxDays").addClass('active');
+        }
+        else if (dateVal === $("#twoDays").val()) {
+            $("#aTwoDays").addClass('active');
+        }
+        else if (dateVal === $("#sevenDays").val()) {
+            $("#aSevenDays").addClass('active');
+        }
+    })
+});
