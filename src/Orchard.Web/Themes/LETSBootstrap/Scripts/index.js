eb93fddd029a2447019c34213d1d970c1027289a
marshy <marshy@zeta.com.au>
2019-02-04 14:15:45 +0800

    Extract needed parts of ShapesBase out to theme's webpack build & override some templates to avoid extra script loading

--- src/Orchard.Web/Themes/LETSBootstrap/Scripts/index.js
+++ src/Orchard.Web/Themes/LETSBootstrap/Scripts/index.js
@@ -11,6 +11,52 @@ if (process.env.NODE_ENV !== 'production') {
     console.log('Looks like we are in development mode!');
 }
 
+// UnsafeUrl links -> form POST
+$(function () {
+    var magicToken = $("input[name=__RequestVerificationToken]").first();
+    if (!magicToken) { return; } // no sense in continuing if form POSTS will fail
+    $("body").on("click", "a[itemprop~=UnsafeUrl], a[data-unsafe-url]", function () {
+        var _this = $(this);
+        var hrefParts = _this.attr("href").split("?");
+        var form = $("<form action=\"" + hrefParts[0] + "\" method=\"POST\" />");
+        form.append(magicToken.clone());
+        if (hrefParts.length > 1) {
+            var queryParts = hrefParts[1].split("&");
+            for (var i = 0; i < queryParts.length; i++) {
+                var queryPartKVP = queryParts[i].split("=");
+                //trusting hrefs in the page here
+                form.append($("<input type=\"hidden\" name=\"" + decodeURIComponent(queryPartKVP[0]) + "\" value=\"" + decodeURIComponent(queryPartKVP[1]) + "\" />"));
+            }
+        }
+        form.css({ "position": "absolute", "left": "-9999em" });
+        $("body").append(form);
+
+        var unsafeUrlPrompt = _this.data("unsafe-url");
+
+        if (unsafeUrlPrompt && unsafeUrlPrompt.length > 0) {
+            if (!confirm(unsafeUrlPrompt)) {
+                return false;
+            }
+        }
+
+        if (_this.filter("[itemprop~='RemoveUrl']").length == 1) {
+            // use a custom message if its set in data-message
+            var dataMessage = _this.data('message');
+            if (dataMessage === undefined) {
+                dataMessage = "Are you sure you want to delete this?";
+            }
+
+            if (!confirm(dataMessage)) {
+                return false;
+            }
+        }
+
+        form.submit();
+        return false;
+
+    });
+});
+
 // this is needed for bootstrap4 validation to work well with jquery unobtrusive
 window.addEventListener('load', function () {
     var forms = document.getElementsByClassName('needs-validation');
